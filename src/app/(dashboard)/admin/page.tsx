c33a5d7d9fb45346949440a6ab322c98b6ecae4c
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-09-16 12:27:15 -0300

    feat: implement AdminPage layout with UseCard component for user types

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import UseCard from '@/components/UseCard';
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className='w-full lg:w-2/3'>
+        {/* USER CARD */}
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <UseCard type='Student' />
+          <UseCard type='Techer' />
+          <UseCard type='Parent' />
+          <UseCard type='Staff' />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3'>RIGHT</div>
+    </div>
+  );
 };
 
 export default AdminPage;
