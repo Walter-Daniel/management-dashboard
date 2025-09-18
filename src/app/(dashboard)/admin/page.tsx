7e78e2e3928314edc1846a87852c85bbb14c2810
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-09-18 11:03:37 -0300

    feat: add FinanceChart component and integrate it into AdminPage layout

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UseCard from '@/components/UseCard';
 
 const AdminPage = () => {
@@ -26,7 +27,9 @@ const AdminPage = () => {
           </div>
         </div>
         {/* BOTTOM CHART */}
-        <div></div>
+        <div className='w-full h-[500px]'>
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
       <div className='w-full lg:w-1/3'>RIGHT</div>
