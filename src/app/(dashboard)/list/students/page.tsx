0288290d2daf9ceb2b771c1f87d5fd7f0888bc7f
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-10-14 11:30:54 -0300

    feat: update StudentListPage headers and improve layout for better clarity

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -23,34 +23,24 @@ const columns = [
     accesor: 'info',
   },
   {
-    header: 'Teacher',
+    header: 'Estudiante ID',
     accesor: 'studentId',
     className: 'hidden md:table-cell',
   },
   {
-    header: 'Grade',
+    header: 'Grado',
     accesor: 'grade',
     className: 'hidden md:table-cell',
   },
   {
-    header: 'Subjects',
-    accesor: 'grade',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Classes',
-    accesor: 'classes',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Phone',
+    header: 'Teléfono',
     accesor: 'phone',
-    className: 'hidden lg:table-cell',
+    className: 'hidden md:table-cell',
   },
   {
-    header: 'Address',
+    header: 'Dirección',
     accesor: 'address',
-    className: 'hidden lg:table-cell',
+    className: 'hidden md:table-cell',
   },
   {
     header: 'Actions',
@@ -105,7 +95,7 @@ const StudentListPage = () => {
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
       {/* TOP */}
       <div className='flex items-center justify-between'>
-        <h1 className='hidden md:block text-lg font-semibold'>Profesores</h1>
+        <h1 className='hidden md:block text-lg font-semibold'>Estudiantes</h1>
         <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
           <TableSearch />
           <div className='flex items-center gap-4 self-end'>
