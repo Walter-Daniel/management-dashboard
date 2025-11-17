3d4e75967b735d6d26b2e158afc5c8bb4433b9f0
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-11-17 17:26:20 -0300

    feat: enhance role-based access control in Student and Teacher list pages; update auth handling and improve data rendering

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -2,92 +2,98 @@ import FormModal from '@/components/FormModal';
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, studentsData } from '@/lib/data';
 import { prisma } from '@/lib/prisma';
 import { ITEM_PER_PAGE } from '@/lib/settings';
+import { auth } from '@clerk/nextjs/server';
 import { Class, Prisma, Student } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { de } from 'zod/locales';
 
 type StudentList = Student & { class: Class };
-
-const columns = [
-  {
-    header: 'Info',
-    accesor: 'info',
-  },
-  {
-    header: 'Student ID',
-    accesor: 'studentId',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Grade',
-    accesor: 'grade',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Phone',
-    accesor: 'phone',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Address',
-    accesor: 'address',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'Actions',
-    accesor: 'actions',
-  },
-];
-
-const renderRows = (item: StudentList) => (
-  <tr
-    key={item.id}
-    className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-schoolPurpleLight'
-  >
-    <td className='flex items-center gap-4 p-4'>
-      <Image
-        src={item.image || '/noAvatar.png'}
-        alt={`${item.firstName}'s avatar`}
-        width={40}
-        height={40}
-        className='md:hidden xl:block w-10 h-10 rounded-full object-cover'
-      />
-      <div className='flex flex-col'>
-        <h3 className='font-semibold'>{item.firstName}</h3>
-        <p className='text-xs text-gray-500'>{item.class.name}</p>
-      </div>
-    </td>
-    <td className='hidden md:table-cell'>#{item.username}</td>
-    <td className='hidden md:table-cell'>{item.class.name[0]}</td>
-    <td className='hidden md:table-cell'>{item?.phone}</td>
-    <td className='hidden md:table-cell'>{item.address}</td>
-    <td>
-      <div className='flex items-center gap-2'>
-        <Link
-          href={`/list/students/${item?.id}`}
-          className='btn btn-sm bg-schoolBlue hover:bg-schoolBlue/80 text-white'
-        >
-          <button className='w-7 h-7 flex items-center justify-center rounded-full bg-schoolSky'>
-            <Image src='/filter.png' alt='' width={14} height={14} />
-          </button>
-        </Link>
-        {role === 'admin' && (
-          <FormModal table='student' type='delete' id={item.id} />
-        )}
-      </div>
-    </td>
-  </tr>
-);
 
 const StudentListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+
+  const columns = [
+    {
+      header: 'Info',
+      accesor: 'info',
+    },
+    {
+      header: 'Student ID',
+      accesor: 'studentId',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Grade',
+      accesor: 'grade',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Phone',
+      accesor: 'phone',
+      className: 'hidden md:table-cell',
+    },
+    {
+      header: 'Address',
+      accesor: 'address',
+      className: 'hidden md:table-cell',
+    },
+    ...(role === 'admin'
+      ? [
+          {
+            header: 'Actions',
+            accesor: 'actions',
+          },
+        ]
+      : []),
+  ];
+
+  const renderRows = (item: StudentList) => (
+    <tr
+      key={item.id}
+      className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-schoolPurpleLight'
+    >
+      <td className='flex items-center gap-4 p-4'>
+        <Image
+          src={item.image || '/noAvatar.png'}
+          alt={`${item.firstName}'s avatar`}
+          width={40}
+          height={40}
+          className='md:hidden xl:block w-10 h-10 rounded-full object-cover'
+        />
+        <div className='flex flex-col'>
+          <h3 className='font-semibold'>{item.firstName}</h3>
+          <p className='text-xs text-gray-500'>{item.class.name}</p>
+        </div>
+      </td>
+      <td className='hidden md:table-cell'>#{item.username}</td>
+      <td className='hidden md:table-cell'>{item.class.name[0]}</td>
+      <td className='hidden md:table-cell'>{item?.phone}</td>
+      <td className='hidden md:table-cell'>{item.address}</td>
+      <td>
+        <div className='flex items-center gap-2'>
+          <Link
+            href={`/list/students/${item?.id}`}
+            className='btn btn-sm bg-schoolBlue hover:bg-schoolBlue/80 text-white'
+          >
+            <button className='w-7 h-7 flex items-center justify-center rounded-full bg-schoolSky'>
+              <Image src='/filter.png' alt='' width={14} height={14} />
+            </button>
+          </Link>
+          {role === 'admin' && (
+            <FormModal table='student' type='delete' id={item.id} />
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   const { page, ...queryParams } = searchParams;
   const p = page ? Number(page) : 1;
 
