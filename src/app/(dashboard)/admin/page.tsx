f6a104583e5174b47034de372420326512777c53
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-11-17 21:58:40 -0300

    feat: refactor admin dashboard components; replace CountChart with CountChartContainer and update UseCard to fetch dynamic data

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,6 @@
 import Announcements from '@/components/Announcements';
 import AttendanceChart from '@/components/AttendanceChart';
-import CountChart from '@/components/CountChart';
+import CountChartContainer from '@/components/CountChartContainer';
 import EventCalendar from '@/components/EventCalendar';
 import FinanceChart from '@/components/FinanceChart';
 import UseCard from '@/components/UseCard';
@@ -12,16 +12,16 @@ const AdminPage = () => {
       <div className='w-full lg:w-2/3 flex flex-col gap-8'>
         {/* USER CARD */}
         <div className='flex gap-4 justify-between flex-wrap'>
-          <UseCard type='Student' key={1} />
-          <UseCard type='Techer' key={2} />
-          <UseCard type='Parent' key={3} />
-          <UseCard type='Staff' key={4} />
+          <UseCard type='admin' key={4} />
+          <UseCard type='teacher' key={2} />
+          <UseCard type='student' key={1} />
+          <UseCard type='parent' key={3} />
         </div>
         {/* MIDDLE CHART */}
         <div className='flex gap-4 flex-col lg:flex-row'>
           {/* COUNT CHART */}
           <div className='w-full lg:w-1/3 h-[450px]'>
-            <CountChart />
+            <CountChartContainer />
           </div>
           {/* ATTENDANCE CHART */}
           <div className='w-full lg:w-2/3 h-[450px]'>
