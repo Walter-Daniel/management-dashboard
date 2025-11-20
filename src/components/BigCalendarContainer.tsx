5e3434506c9957a4b705f8a414ea7edbc445a37b
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-11-20 17:05:22 -0300

    feat: implement BigCalendarContainer for dynamic lesson scheduling;

---
+++ src/components/BigCalendarContainer.tsx
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma';
+import BigCalendar from './BigCalendar';
+
+const BigCalendarContainer = async ({
+  type,
+  id,
+}: {
+  type: 'teacherId' | 'classId';
+  id: string | number;
+}) => {
+  const response = await prisma.lesson.findMany({
+    where: {
+      ...(type === 'teacherId'
+        ? { teacherId: id as string }
+        : { classId: id as number }),
+    },
+  });
+
+  const data = response.map((lesson) => ({
+    title: lesson.title,
+    start: lesson.startTime,
+    end: lesson.endTime,
+  }));
+
+  return (
+    <div>
+      <BigCalendar data={data} />
+    </div>
+  );
+};
+
+export default BigCalendarContainer;
