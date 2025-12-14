f464e63934c142f07fa1950fed0a20746490f035
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-12-14 18:54:00 -0300

    feat: implement SingleStudentPage with student details and attendance card, refactor BigCalendar integration

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Announcements from '@/components/Announcements';
-import BigCalendar from '@/components/BigCalendar';
+import BigCalendarContainer from '@/components/BigCalendarContainer';
 import FormContainer from '@/components/FormContainer';
 import Performance from '@/components/Performance';
 import prisma from '@/lib/prisma';
@@ -181,7 +181,7 @@ const SingleTeacherPage = async ({ params }: { params: { id: string } }) => {
         {/* BOTTOM */}
         <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
           <h1 className='text-xl font-semibold'>Teacher&apos;s Schedule</h1>
-          {/* <BigCalendar /> */}
+          <BigCalendarContainer type='teacherId' id={teacher.id} />
         </div>
       </div>
       {/* RIGHT */}
